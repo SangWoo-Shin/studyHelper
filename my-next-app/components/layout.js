25423e498f96bc64f4af6f3b4af986f99576ceab
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2025-01-31 23:45:24 -0500

    Update layout.js

--- my-next-app/components/layout.js
+++ my-next-app/components/layout.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Provider from './provider';
-//import styles from '@/styles/Home.module.css';
-//import SideBar from './sideBar';
 
 const Layout = (props) => {
-
     return (
-        <div>
-            <Container>
-                <Provider>
-                    {props.children}
-                </Provider>
-            </Container>
-        </div>
+        <Container>
+                {props.children}
+        </Container>
     );
 };
 
