47af81eb34e1ed1c8e584832d3a3d9577ac2947d
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2025-01-31 23:45:55 -0500

    Create menu.js

---
+++ my-next-app/components/menu.js
@@ -0,0 +1,56 @@
+import styles from '../styles/chat.module.css';
+import { useSession, signOut } from "next-auth/react";
+
+const Menu = () => {
+    const { data: session, status } = useSession();
+
+    const handleSignOut = async () => {
+        const result = await signOut({ callbackUrl: "/user/login", redirect: false });
+        if(!result?.url) {
+            console.error("Sign-out redirection failed");
+        }
+    };
+
+    if(status === "loading") {
+        return <p>Loading...</p>
+    }
+
+  return (
+    <>
+    {session ? (
+    <nav className={styles.menu}>
+      <ul className={styles.items}>
+        <li className={`${styles.item}`}>
+          <i className="fa fa-home" aria-hidden="true"></i>
+        </li>
+        <li className={`${styles.item}`}>
+          <i className="fa fa-user" aria-hidden="true"></i>
+        </li>
+        <li className={`${styles.item}`}>
+          <i className="fa fa-pencil" aria-hidden="true"></i>
+        </li>
+        <li className={`${styles.item} ${styles.active}`}>
+          <i className="fa fa-commenting" aria-hidden="true"></i>
+        </li>
+        <li className={`${styles.item}`}>
+          <i className="fa fa-file" aria-hidden="true"></i>
+        </li>
+        <li className={`${styles.item}`}>
+          <i className="fa fa-cog" aria-hidden="true"></i>
+        </li>
+        <button
+            className={styles.signOutButton}
+            onClick={() => handleSignOut()}
+          >
+            Sign Out
+          </button>
+      </ul>
+    </nav>
+    ) : (
+        <LoginPage/>
+    )}
+    </>
+  );
+};
+
+export default Menu;
