d3f883f8def1a108e7b283d2f92f5f401a5512a9
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2025-01-31 23:49:06 -0500

    Update index.js

--- my-next-app/pages/index.js
+++ my-next-app/pages/index.js
@@ -1,36 +1,20 @@
-import { useSession, signOut } from "next-auth/react";
-import LoginPage from '../pages/user/login';
 import styles from "../styles/home.module.css";  // Assuming this stylesheet exists
+import SideBar from '../components/sideBar';
+import Menu from "../components/menu";
+import DiscussionList from "../components/discussionList";
+import Chat from "../components/chat";
+import { Container } from 'react-bootstrap';
 
  const Home = () => {
-  const { data: session, status } = useSession();
-
-  const handleSignOut = async () => {
-    const result = await signOut({ callbackUrl: "/user/login", redirect: false });
-    if (!result?.url) {
-      console.error("Sign-out redirection failed");
-    }
-  };
-
-  if (status === "loading") {
-    return <p>Loading...</p>; // Handle loading state
-  }
-
   return (
     <>
-      {session ? (
-        <div className={styles.container}>
-          <p>Welcome, {session.user.name}!</p>
-          <button
-            className={styles.signOutButton}
-            onClick={() => handleSignOut()}
-          >
-            Sign Out
-          </button>
-        </div>
-      ) : (
-        <LoginPage/>
-      )}
+      (
+        <Container className={styles.Container}>
+            <Menu />
+            <DiscussionList />
+            <Chat />
+        </Container>
+      )
     </>
   );
 }
